738c18923a1bfc7fa12b9dee76aefd0c61f787d5
evan <evanan39@yahoo.com>
2019-04-01 23:38:10 -0400

    cleanup

--- app.js
+++ app.js
@@ -14,12 +14,13 @@ https.get(mediaURL, (res) => {
     res.on("end", () => {
         let mediaList = JSON.parse(data);
         let outDir = "./out";
-        if (!fs.existsSync(outDir)){
+        if (!fs.existsSync(outDir)) {
             fs.mkdirSync(outDir);
         }
         mediaList.forEach((media) => {
-            let file = fs.createWriteStream(`out/${media.media_details.sizes.full.file}`);
-            https.get(media.media_details.sizes.full.source_url, (res) => {
+            let full = media.media_details.sizes.full;
+            let file = fs.createWriteStream(`${outDir}/${full.file}`);
+            https.get(full.source_url, (res) => {
                 res.pipe(file);
             });
         });
